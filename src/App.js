b3b8115b5e3e50bc79e3b4206528054e3421e386
Kemal Sezgen <kemalsezgen1@gmail.com>
2022-03-31 20:13:53 +0300

    get request and view the data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+
+  const [conracts, setConracts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://seffaflik.epias.com.tr/transparency/service/market/intra-day-trade-history?endDate=2022-01-26&startDate=2022-01-26")
+      .then((res) => setConracts(res.data.body.intraDayTradeHistoryList))
+      .catch((error) => console.log(error));
+  });
+
+  
+
   return (
     <div className="App">
       smartPulse - Coding Task
+      <p>Conract Sayısı = {conracts.length}</p>
+      <div>
+        {conracts.map((conract) => {
+          return (
+            <div key={conract.id}>
+              <h2>Conract ID: {conract.id}</h2>
+              <h2>Conract Date: {conract.date}</h2>
+              <h2>Conract Name: {conract.conract}</h2>
+              <h2>Price: {conract.price}</h2>
+              <h2>Quantity: {conract.quantity}</h2>
+              <h2>Conract Type: {conract.conract.substring(0,2)}</h2>
+              <h2>----------------------------------------</h2>
+            </div>
+          )
+        })}
+      </div>
     </div>
   );
 }
