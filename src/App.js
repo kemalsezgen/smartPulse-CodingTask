019b66a96a636eb522dc6388b9dfadf634f2acfe
Kemal Sezgen <kemalsezgen1@gmail.com>
2022-04-04 05:21:19 +0300

    diffirent file for table component and added date pickers

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import "./App.css";
+import Table from "./Table";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 function App() {
+  const startD = new Date("2022-01-26");
+  const endD = new Date("2022-01-26");
+
   const [data, setData] = useState([]); // all data
+  const [startDateValue, setStartDateValue] = useState(startD);
+  const [endDateValue, setEndDateValue] = useState(endD);
+
+  const startDText =
+    startDateValue.getFullYear() +
+    "-" +
+    (startDateValue.getMonth() + 1) +
+    "-" +
+    startDateValue.getDate();
+  const endDText =
+    endDateValue.getFullYear() +
+    "-" +
+    (endDateValue.getMonth() + 1) +
+    "-" +
+    endDateValue.getDate();
 
   // Get request
   useEffect(() => {
     axios
       .get(
-        "https://seffaflik.epias.com.tr/transparency/service/market/intra-day-trade-history?endDate=2022-01-26&startDate=2022-01-26"
+        `https://seffaflik.epias.com.tr/transparency/service/market/intra-day-trade-history?endDate=${endDText}&startDate=${startDText}`
       )
       .then((res) => setData(res.data.body.intraDayTradeHistoryList))
       .catch((error) => console.log(error));
@@ -20,22 +41,17 @@ function App() {
     (data) => data.conract.substring(0, 2) === "PH"
   );
 
-  // sayıların virgülden sonraki kısımlarını düzenleyen fonksiyon
-  const roundNumber = (number) => {
-    return Math.round(number * 100) / 100;
-  };
-
   // tabloda listelenecek olan tarihi key, bilgileri ise value olarak tutan dictionary
   const PH_VALUES = {};
 
   for (let i = 0; i < filteredData.length; i++) {
-    var toplamIslemMiktari = filteredData[i].quantity / 10  
-    var toplamIslemTutari = filteredData[i].price * toplamIslemMiktari
-    var agirlikliOrtFiyat = toplamIslemTutari / toplamIslemMiktari
+    var toplamIslemMiktari = filteredData[i].quantity / 10;
+    var toplamIslemTutari = filteredData[i].price * toplamIslemMiktari;
+    var agirlikliOrtFiyat = toplamIslemTutari / toplamIslemMiktari;
 
     // çekilen verideki "date" değerini kullanarak yeni bir date değişkeni oluşturup bu date'in formatını değiştirme
     const date = new Date(filteredData[i].date);
-    const fDate =  
+    const fDate =
       date.getDate() +
       "." +
       (date.getMonth() + 1) +
@@ -56,12 +72,14 @@ function App() {
       filteredData[i].conract.substring(8, 10) +
       ":00"; */
 
-    if (fDate in PH_VALUES) {  // eğer bu date sözlükte key olarak bulunuyorsa value'leri arttır
+    if (fDate in PH_VALUES) {
+      // eğer bu date sözlükte key olarak bulunuyorsa value'leri arttır
       PH_VALUES[fDate][0] += toplamIslemMiktari;
       PH_VALUES[fDate][1] += toplamIslemTutari;
       PH_VALUES[fDate][2] += agirlikliOrtFiyat;
     } else {
-      PH_VALUES[fDate] = [  // bulunmuyorsa yeni bir key-value oluştur.
+      PH_VALUES[fDate] = [
+        // bulunmuyorsa yeni bir key-value oluştur.
         toplamIslemMiktari,
         toplamIslemTutari,
         agirlikliOrtFiyat,
@@ -71,9 +89,9 @@ function App() {
 
   return (
     <div className="App">
-      <h2>smartPulse - Coding Task</h2>
+      <h1 style={{color: "#4D5858"}}>smart<span style={{color:"#AEE03F",}}>Pulse</span> - Coding Task</h1>
 
-      <h3 style={{ border: "1px solid black", marginBottom:"20px"}}>
+      <h3 style={{ border: "1px solid black", marginBottom: "20px" }}>
         Tarihleri parse ederken direkt gelen data içerisindeki "date"
         özelliğinden yararlandım. Çünkü PH22012722 isimli bir conract kurallara
         göre 27 Ocak tarihli olması gerekirken date özelliği 26 Ocak olarak
@@ -82,33 +100,29 @@ function App() {
         ise yorum satırlarıyla kodda bulabilirsiniz.
       </h3>
 
-      <table
-        style={{
-          border: "solid 1px red",
-          marginLeft: "auto",
-          marginRight: "auto",
-          fontSize: "20px",
-          background: "skyblue",
-        }}
-      >
-        <tr style={{background:"grey"}}>
-          <th>Tarih</th>
-          <th>Toplam İşlem Miktarı (MWh)</th>
-          <th>Toplam İşlem Tutarı (TL)</th>
-          <th>Ağırlık Ortalama Fiyat (TL/MWh)</th>
-        </tr>
-        {Object.keys(PH_VALUES).map((key, index) => {  // sözlüğün map ile dönülüp değerlerin tabloya yerleştirilmesi
-          return (
-            <tr key={index}>
-              <td>{key}</td>
-              <td>{roundNumber(PH_VALUES[key][0])}</td>
-              <td>{roundNumber(PH_VALUES[key][1])}</td>
-              <td>{roundNumber(PH_VALUES[key][2])}</td>
-            </tr>
-          );
-        })}
-      </table>
-      {filteredData.map((data) => { // PH ile başlayan tüm veriler. İncelemek, üzerinde düşünmek için bastırıldı. İsteğe göre yorum satırına alınabilir.
+      <div className="temp">
+        <h4>Start Date:</h4>
+        <DatePicker
+          selected={startDateValue}
+          onChange={(date) => setStartDateValue(date)}
+          dateFormat="yyyy/MM/dd"
+        />
+
+        <h4>End Date:</h4>
+        <DatePicker
+          selected={endDateValue}
+          onChange={(date) => setEndDateValue(date)}
+          dateFormat="yyyy/MM/dd"
+        />
+      </div>
+
+      <h3>Veri sayısı çok fazla olduğundan 2-3 günden uzun aralıklar seçmemeniz tavsiye edilir.</h3>
+
+      {/* Table componentini ayrı dosyada oluşturup props yardımıyla sözlüğü componente ilettim. */}
+      <Table data={PH_VALUES} />
+
+      {/*       {filteredData.map((data) => {
+        // PH ile başlayan tüm veriler. İncelemek, üzerinde düşünmek için bastırıldı. İsteğe göre yorum satırına alınabilir.
         return (
           <div key={data.id} style={{ border: "1px solid grey" }}>
             <h2>Conract = {data.conract}</h2>
@@ -117,7 +131,7 @@ function App() {
             <h2>date = {data.date}</h2>
           </div>
         );
-      })}
+      })} */}
     </div>
   );
 }
---
+++ src/Table.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+const Table = (props) => {
+
+  // sayıların virgülden sonraki kısımlarını düzenleyen fonksiyon
+  const roundNumber = (number) => {
+    return Math.round(number * 100) / 100;
+  };
+
+  return (
+    <div>
+      <table
+        style={{
+          border: "solid 1px red",
+          marginLeft: "auto",
+          marginRight: "auto",
+          fontSize: "20px",
+          background: "#AEE03F",
+        }}
+      >
+        <tr style={{ background: "grey" }}>
+          <th>Tarih</th>
+          <th>Toplam İşlem Miktarı (MWh)</th>
+          <th>Toplam İşlem Tutarı (TL)</th>
+          <th>Ağırlık Ortalama Fiyat (TL/MWh)</th>
+        </tr>
+        {Object.keys(props.data).map((key, index) => {
+          // sözlüğün map ile dönülüp değerlerin tabloya yerleştirilmesi
+          return (
+            <tr key={index}>
+              <td>{key}</td>
+              <td>{roundNumber(props.data[key][0])}</td>
+              <td>{roundNumber(props.data[key][1])}</td>
+              <td>{roundNumber(props.data[key][2])}</td>
+            </tr>
+          );
+        })}
+      </table>
+    </div>
+  );
+};
+
+export default Table;
